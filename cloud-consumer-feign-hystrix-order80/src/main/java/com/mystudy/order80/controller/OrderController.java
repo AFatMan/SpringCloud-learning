3cb68621db66d30a6ddd79f70302ed4d275b70d1
AFatMan <1453052796@qq.com>
2021-03-16 10:07:02 +0800

    +:Hystrix消费端全局服务降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/mystudy/order80/controller/OrderController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/mystudy/order80/controller/OrderController.java
@@ -1,8 +1,8 @@
 package com.mystudy.order80.controller;
 
 import com.mystudy.order80.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  */
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback") // 配置全局服务降级
 public class OrderController {
 
     @Autowired
@@ -24,18 +25,29 @@ public class OrderController {
         return paymentHystrixService.paymentInfo_OK(id);
     }
 
-    @HystrixCommand(
-            // 当调用此方法发生服务熔断时,兜底方法的方法名
-            fallbackMethod = "fallbackTimeOut",
-            commandProperties = {
-                    // 超时时间
-                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000")}
-    )
+
+    // 使用全局服务降级
+    // @HystrixCommand(
+    //         // 当调用此方法发生服务熔断时,兜底方法的方法名
+    //         fallbackMethod = "fallbackTimeOut",
+    //         commandProperties = {
+    //                 // 超时时间
+    //                 @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000")}
+    // )
+    @HystrixCommand
     @GetMapping("consumer/payment/hystrix/timeout/{id}")
     public String timeOut(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfo_TimeOut(id);
     }
     public String fallbackTimeOut(Integer id) {
         return "服务降级: 系统繁忙!!!^(*￣(oo)￣)^ ";
+    }
+
+    /**
+     * 全局降级方法,与业务逻辑混杂,有待改进
+     * @return
+     */
+    public String defaultFallback() {
+        return "OrderController全局兜底:系统繁忙!!!^(*￣(oo)￣)^ ";
     }
 }
