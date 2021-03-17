b92d0d675f042e38d0d1f0b5dc44adb7d7ee0e85
AFatMan <1453052796@qq.com>
2021-03-17 11:32:38 +0800

    +:gateway常用的Predicate

--- cloud-gateway-gateway9527/src/main/java/com/mystudy/gateway/CloudGateway9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/mystudy/gateway/CloudGateway9527.java
@@ -5,6 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
+ * 常用的Route Predicate Factory
+ *      The After Route Predicate Factory
+ *      The Before Route Predicate Factory
+ *      The Between Route Predicate Factory
+ *      The Cookie Route Predicate Factory
+ *      The Header Route Predicate Factory
+ *      The Host Route Predicate Factory
+ *      The Method Route Predicate Factory
+ *      The Path Route Predicate Factory
+ *      The Query Route Predicate Factory
+ *      The RemoteAddr Route Predicate Factory
+ *      The weight Route Predicate Factory
+ *
  * @Author 刘健生
  * @Date 2021-03-16 16:41
  * @Description
