8357864bad7a18b446b2939f335ba2b8e8695f47
AFatMan <1453052796@qq.com>
2021-03-22 17:45:32 +0800

    +:SpringCloudAlibaba-nacos服务提供者

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/mystudy/provider/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.mystudy.provider.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 刘健生
+ * @Date 2021-03-22 17:22
+ * @Description
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
---
+++ cloudalibaba-provider-payment9002/src/main/java/com/mystudy/provider/AliProviderPayment9002.java
@@ -0,0 +1,20 @@
+package com.mystudy.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 刘健生
+ * @Date 2021-03-22 17:20
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AliProviderPayment9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliProviderPayment9002.class,args);
+    }
+
+}
