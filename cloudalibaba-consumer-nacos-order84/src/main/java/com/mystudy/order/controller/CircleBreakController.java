c6080677d65379fa98647130a237e792eeac0f72
AFatMan <1453052796@qq.com>
2021-03-24 15:43:54 +0800

    +:SpringCloudAlibaba-sentinel熔断功能:消费者&生产者项目初始化(测试方法)

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/mystudy/order/controller/CircleBreakController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/mystudy/order/controller/CircleBreakController.java
@@ -1,5 +1,10 @@
 package com.mystudy.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.mystudy.cloudapicommon.entity.CommonResult;
+import com.mystudy.cloudapicommon.entity.Payment;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,4 +21,19 @@ public class CircleBreakController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @RequestMapping("/consumer/fallback/{id}")
+    @SentinelResource(value = "fallback")//没有配置
+    public CommonResult<Payment> fallback(@PathVariable Long id)
+    {
+        CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/"+id,CommonResult.class,id);
+
+        if (id == 4) {
+            throw new IllegalArgumentException ("IllegalArgumentException,非法参数异常....");
+        }else if (result.getData() == null) {
+            throw new NullPointerException ("NullPointerException,该ID没有对应记录,空指针异常");
+        }
+
+        return result;
+    }
 }
